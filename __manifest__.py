14c84e4545845b73cfa3ebd711e5337377c38d5b
dinoherlambang <dino.herlambang@gmail.com>
2026-08-05 17:50:58 +0700

    rename the module

--- __manifest__.py
+++ __manifest__.py
@@ -18,6 +18,7 @@ TMS Fleet & Tire Intelligence Dashboard
     'depends': [
         'base',
         'web',
+        'stock',
         'dh_tms',
     ],
     'data': [
@@ -29,16 +30,6 @@ TMS Fleet & Tire Intelligence Dashboard
     'qweb': [
         'static/src/xml/tms_intelligence_dashboard_templates.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'dh_tms_intelligence_dashboard/static/src/css/tms_intelligence_dashboard.css',
-            'dh_tms_intelligence_dashboard/static/src/scss/tms_intelligence_dashboard.scss',
-            'dh_tms_intelligence_dashboard/static/src/js/tms_intelligence_dashboard.js',
-        ],
-        'web.assets_qweb': [
-            'dh_tms_intelligence_dashboard/static/src/xml/tms_intelligence_dashboard_templates.xml',
-        ],
-    },
     'installable': True,
     'application': True,
     'auto_install': False,
